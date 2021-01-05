4431b5247028091cce4792e2fa27f9a9dd6a6ac4
mendoanjoe <mendoanjoe@gmail.com>
2021-01-05 18:09:10 +0700

    delete nodes and relation

--- modules/neo4j/handler.go
+++ modules/neo4j/handler.go
@@ -57,3 +57,45 @@ func (h handler) CreateEdges(ctx iris.Context) {
 	helper.CreateResponse(ctx).Ok().JSON()
 	ctx.Next()
 }
+
+func (h handler) DeleteNodes(ctx iris.Context) {
+	request := ctx.Values().Get("body").(*CreateRequestNodes)
+
+	// Create Bulk Node
+	if len(request.Nodes) > 0 {
+		for _, node := range request.Nodes {
+			err := h.neo4j.DeleteNode(node.Label, node.Properties)
+			if err != nil {
+				helper.
+					CreateErrorResponse(ctx, err).
+					InternalServer().
+					JSON()
+				return
+			}
+		}
+	}
+
+	helper.CreateResponse(ctx).Ok().JSON()
+	ctx.Next()
+}
+
+func (h handler) DeleteRelation(ctx iris.Context) {
+	request := ctx.Values().Get("body").(*CreateRequestNodes)
+
+	// Create Bulk Node
+	if len(request.Nodes) > 0 {
+		for _, node := range request.Nodes {
+			err := h.neo4j.DeleteRelation(node.Label, node.Properties)
+			if err != nil {
+				helper.
+					CreateErrorResponse(ctx, err).
+					InternalServer().
+					JSON()
+				return
+			}
+		}
+	}
+
+	helper.CreateResponse(ctx).Ok().JSON()
+	ctx.Next()
+}
