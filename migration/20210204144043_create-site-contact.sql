d8131db2499bc01d68f142e6305f283471e27dc0
Ganesha Danu <ganeshadanu1@gmail.com>
2021-02-04 16:39:43 +0700

    add site contact migration

---
+++ migration/20210204144043_create-site-contact.sql
@@ -0,0 +1,13 @@
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS "site_contacts" (
+    "id" UUID PRIMARY KEY NOT NULL,
+    "site_id" UUID NOT NULL,
+    "contact_id" UUID NOT NULL,
+    "created_at" TIMESTAMP,
+    "updated_at" TIMESTAMP,
+    "deleted_at" TIMESTAMP
+
+);
+
+-- +migrate Down
+DROP TABLE IF EXISTS "site_contacts";
