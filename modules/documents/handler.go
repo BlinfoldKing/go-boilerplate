ec7cda88b2e02c97a6bfe707c6a015eb320c2022
IlhamRobyana <ilhamrobyanasofyan@gmail.com>
2021-02-10 20:48:22 +0700

    List, Update, Delete Documents

--- modules/documents/handler.go
+++ modules/documents/handler.go
@@ -1,6 +1,7 @@
 package documents
 
 import (
+	"fmt"
 	"go-boilerplate/adapters"
 	"go-boilerplate/entity"
 	"go-boilerplate/helper"
@@ -11,6 +12,20 @@ import (
 type handler struct {
 	documents Service
 	adapters  adapters.Adapters
+}
+
+func (h handler) GetList(ctx iris.Context) {
+	request := ctx.Values().Get("pagination").(entity.Pagination)
+	documents, count, err := h.documents.GetList(request)
+	if err != nil {
+		helper.
+			CreateErrorResponse(ctx, err).
+			InternalServer().
+			JSON()
+		return
+	}
+	helper.CreatePaginationResponse(ctx, request, documents, count).JSON()
+	ctx.Next()
 }
 
 func (h handler) Upload(ctx iris.Context) {
@@ -72,6 +87,41 @@ func (h handler) GetByID(ctx iris.Context) {
 		return
 	}
 
+	helper.CreateResponse(ctx).Ok().WithData(document).JSON()
+	ctx.Next()
+}
+
+func (h handler) DeleteByID(ctx iris.Context) {
+	id := ctx.Params().GetString("id")
+	err := h.documents.DeleteByID(id)
+	if err != nil {
+		helper.
+			CreateErrorResponse(ctx, err).
+			InternalServer().
+			JSON()
+		return
+	}
+	helper.CreateResponse(ctx).Ok().WithMessage(fmt.Sprintf("%s deleted", id)).JSON()
+	ctx.Next()
+}
+func (h handler) Update(ctx iris.Context) {
+	request := ctx.Values().Get("body").(*UpdateRequest)
+	id := ctx.Params().GetString("id")
+	document, err := h.documents.Update(id, entity.DocumentChangeSet{
+		Name:       request.Name,
+		FileSize:   request.FileSize,
+		FileType:   request.FileType,
+		ObjectName: request.ObjectName,
+		BucketName: request.BucketName,
+		URLLink:    request.URLLink,
+	})
+	if err != nil {
+		helper.
+			CreateErrorResponse(ctx, err).
+			InternalServer().
+			JSON()
+		return
+	}
 	helper.CreateResponse(ctx).Ok().WithData(document).JSON()
 	ctx.Next()
 }
--- modules/documents/routes.go
+++ modules/documents/routes.go
@@ -2,6 +2,7 @@ package documents
 
 import (
 	"go-boilerplate/adapters"
+	"go-boilerplate/middlewares"
 
 	"github.com/kataras/iris/v12"
 )
@@ -18,6 +19,9 @@ func Routes(prefix iris.Party, adapters adapters.Adapters) {
 	documents.Post(":upload", handler.Upload)
 	documents.Post(":download", handler.Download)
 
+	documents.Get("/", middlewares.ValidatePaginationQuery, handler.GetList)
 	documents.Get("/{id:string}", handler.GetByID)
 	documents.Post("/", handler.Create)
+	documents.Delete("/{id:string}", handler.DeleteByID)
+	documents.Put("/{id:string}", middlewares.ValidateBody(&UpdateRequest{}), handler.Update)
 }
