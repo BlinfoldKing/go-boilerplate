72d978aed48c39fe1f6cc9015b1cdd023319dec9
Ganesha Danu <ganeshadanu1@gmail.com>
2021-02-16 22:45:40 +0700

    fix: error when using in and nin

--- entity/pagination.go
+++ entity/pagination.go
@@ -262,9 +262,9 @@ func getOperation(key string, op string, value interface{}) (res string, err err
 			res = fmt.Sprintf("%s != ?", key)
 		}
 	case "in":
-		res = fmt.Sprintf("%s IN ?", key)
+		res = fmt.Sprintf("%s = ANY(?)", key)
 	case "nin":
-		res = fmt.Sprintf("%s NOT IN ?", key)
+		res = fmt.Sprintf("NOT %s = ANY(?)", key)
 	case "startWith":
 		res = key + " LIKE ? || '%'"
 	case "endWith":
@@ -299,10 +299,17 @@ func parseValueOperator(attribute string, val interface{}) (query string, values
 
 			if v != nil {
 				switch v.(type) {
-				case []string:
-					v = common.StrArr(v.([]string))
 				case []interface{}:
-					v = common.InterfaceArr(v.([]interface{}))
+					switch v.([]interface{})[0].(type) {
+					case string:
+						arr := make(common.StrArr, 0)
+						for _, item := range v.([]interface{}) {
+							arr = append(arr, item.(string))
+						}
+
+						s, _ := arr.ToDB()
+						v = string(s)
+					}
 				}
 				values = append(values, v)
 			}
@@ -320,10 +327,17 @@ func parseValueOperator(attribute string, val interface{}) (query string, values
 
 		if val != nil {
 			switch val.(type) {
-			case []string:
-				val = common.StrArr(val.([]string))
 			case []interface{}:
-				val = common.InterfaceArr(val.([]interface{}))
+				switch val.([]interface{})[0].(type) {
+				case string:
+					arr := make(common.StrArr, 0)
+					for _, item := range val.([]interface{}) {
+						arr = append(arr, item.(string))
+					}
+
+					s, _ := arr.ToDB()
+					val = string(s)
+				}
 			}
 			values = append(values, val)
 		}
